88ca1c01cbca0eb8937bd5558df3a40aabdd1350
Jonas Skjoldan <jonas.skjoldan@gmail.com>
2024-06-13 15:30:49 +0200

    try something else.

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || '/api', // Use environment variable or fallback to `/api`
+  baseURL: '/api', // Use relative URL for API requests
 });
 
 export default axiosInstance;
