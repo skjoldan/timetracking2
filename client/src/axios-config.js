541facbe53bb945b1e356676654668ddc53c2fad
Jonas Skjoldan <jonas.skjoldan@gmail.com>
2024-06-20 12:52:12 +0200

    vercel måske virker.

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: process.env.VUE_APP_API_URL || '/api',
   headers: {
     Authorization: token ? `Bearer ${token}` : ''
   }
