31a3ae538a0d4de7c50f8b904fb7753a08345fbb
Jonas Skjoldan <jonas.skjoldan@gmail.com>
2024-06-26 11:21:42 +0200

    lets try again

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('token');
+const baseURL = process.env.VUE_APP_API_BASE_URL || window.location.origin + '/api'; // Default to the origin if not set
+
+console.log("VUE_APP_API_BASE_URL in axios-config.js:", baseURL);
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: baseURL,
   headers: {
     Authorization: token ? `Bearer ${token}` : ''
   }
 });
-
-console.log("VUE_APP_API_BASE_URL in axios-config.js:", process.env.VUE_APP_API_BASE_URL);
 
 export default axiosInstance;
